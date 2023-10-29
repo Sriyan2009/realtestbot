56e1f109413874f16da4dca0effe41d2d809cdfe
Sriyan2009 <sriyan.yarlagadda@gmail.com>
2023-10-29 12:59:40 -0400

    added code from before
    added code from before

---
+++ src/index.js
@@ -0,0 +1,42 @@
+
+/*
+require("dotenv").config();
+const fs = require('fs');
+
+*/
+/*
+try {
+const { Client, IntentsBitField } = require('discord.js');
+
+const myIntents = new IntentsBitField();
+myIntents.add(IntentsBitField.Flags.GuildPresences, IntentsBitField.Flags.GuildMembers);
+
+const client = new Client({ intents: myIntents });
+} catch(error) {
+    console.log
+}
+*/
+
+console.log("Bot is starting...");
+
+const token = process.env.TOKEN;
+/*
+const users = require("./users.txt");
+
+fs.readFile = ('test.txt', "utf8",(err, data) =>{
+    if(err){
+        console.error
+        return;
+    }
+    console.log(data)
+});
+*/
+
+/*
+try{
+    client.login(token)}
+catch (error) {
+    console.log(error)
+}
+*/
+console.log("Bot has started!");
